f32505965151611c61b636b1b33398ced7e7e8a1
Nikolaev Dmitriy <nik.dima001@yandex.ru>
2022-08-16 15:41:00 +0300

    Flat file comparison (JSON)

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -4,28 +4,37 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+
+import java.nio.file.Path;
 import java.util.concurrent.Callable;
+import static java.nio.file.Files.readString;
+
+import static hexlet.code.Differ.generate;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
 class App implements Callable<String> {
 
     @Parameters(index = "0", description = "path to first file")
-    private String filepath1;
-
+    private Path  filepath1;
     @Parameters(index = "1", description = "path to second file")
-    private String filepath2;
-
+    private Path  filepath2;
     @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
-    private String format = "stylish";
-
+    private String format;
     @Override
     public String call() throws Exception {
-        System.out.println("call is working");
         return "call is working";
     }
-    public static void main(String... args) {
-        int exitCode = new CommandLine(new App()).execute(args);
+    public static void main(String... args) throws Exception {
+        App app = new App();
+        int exitCode = new CommandLine(app).execute(args);
         System.exit(exitCode);
+
+        String string1 = readString(app.filepath1);
+        System.out.println(string1);
+        String string2 = readString(app.filepath2);
+        System.out.println(string2);
+        String diff = Differ.generate(string1, string2);
+        System.out.println(diff);
     }
 }
--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -1,5 +1,17 @@
 package hexlet.code;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 public class Differ {
-
+    public static String generate(String path1, String path2) throws Exception{
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> map1
+                = mapper.readValue(path1, new TypeReference<Map<String, Object>>(){});
+        Map<String, Object> map2
+                = mapper.readValue(path2, new TypeReference<Map<String, Object>>(){});
+        return "call is working";
+    }
 }
