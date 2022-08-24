43321a8e30f78ed458f265c3667c476a0bdbe251
Dmitry Nikolaev <nik.dima001@yandex.ru>
2022-08-24 23:48:32 +0300

    parse json file

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -6,35 +6,39 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.nio.file.Path;
+import java.text.Format;
 import java.util.concurrent.Callable;
+
 import static java.nio.file.Files.readString;
 
-import static hexlet.code.Differ.generate;
-
-@Command(name = "gendiff", mixinStandardHelpOptions = true,
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 class App implements Callable<String> {
 
     @Parameters(index = "0", description = "path to first file")
-    private Path  filepath1;
+    private Path filepath1;
     @Parameters(index = "1", description = "path to second file")
-    private Path  filepath2;
+    private Path filepath2;
     @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
-    private String format;
+    private Format format;
+
     @Override
     public String call() throws Exception {
         return "call is working";
     }
-    public static void main(String... args) throws Exception {
+    public static void main(String[] args) throws Exception {
         App app = new App();
         int exitCode = new CommandLine(app).execute(args);
-        System.exit(exitCode);
 
-        String string1 = readString(app.filepath1);
-        System.out.println(string1);
-        String string2 = readString(app.filepath2);
-        System.out.println(string2);
-        String diff = Differ.generate(string1, string2);
-        System.out.println(diff);
+        if (app.filepath1 == null) {
+            return;
+        }
+
+        String str1 = readString(app.filepath1);
+        System.out.println(str1);
+        String str2 = readString(app.filepath2);
+        System.out.println(str2);
+        String result = Differ.generate(str1, str2);
+        System.out.println(result);
     }
 }
